f140b53967268a22d793a940f3ac25e619863d36
Michi1090 <michi.m1090@gmail.com>
2022-05-23 10:54:24 +0900

    L9-start

---
+++ components/ProductDetails.js
@@ -0,0 +1,11 @@
+app.component('product-details', {
+    props: {
+        details: {
+            type: String,
+            required: true
+        }
+    },
+    template:
+        /*html*/
+        `<div>{{ details }}</div>`
+})
